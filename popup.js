eed66ce7889758b82d41e8b237b38ebcdb4dc54b
morlok1 <inform.cosmo@yandex.ru>
2017-03-21 00:07:02 +0400

    Refactoring. Delete extra files. Add simple UI.

--- popup.js
+++ popup.js
@@ -1,31 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () 
+{
   getStart();
 });
 
-
-
 function getStart()
 {
-	/*
-	var d = document.myForm.sub;
-	d.addEventListener('click', function () {
-  getTranslate();
-});
-	document.addEventListener('select', function () {
-  getSelectionText();
-  });
-  */
-  
-  document.myForm.myText.addEventListener('input', function ()
+  document.translateForm.translateText.addEventListener('input', function ()
   {
-	  /*
-	  e = window.event;
-	  if (e.keyCode == 13)
-	  {
-		  getTranslate();
-	  }
-	  */
-	  getTranslate();
+	getTranslate();
   });
   
   document.addEventListener('submit', function()
@@ -35,48 +17,27 @@ function getStart()
 
 }
 
-function getSelectionText() 
-{
-  var txt = '';
-  if (txt = window.getSelection)
-  {	  // Не IE, используем метод getSelection
-    txt = window.getSelection().toString();
-  } 
-  else 
-  { // IE, используем объект selection
-    txt = document.selection.createRange().text;
-  }
-  alert(txt);
-}
-
 function getTranslate()
 {
-	
-	console.log("---------------------");
-	console.log("Start working...");
 	var key = "dict.1.1.20170318T103102Z.6bf3bd6d3840a4a3.e5ee46ccab2ac06111418309c4034544d15590f4";
-	
+	var langDirect = "en-ru";
 	var url = "https://dictionary.yandex.net/api/v1/dicservice/lookup?" 
 	+ "key=" + key
-	+ "&lang=en-ru"
-	+ "&text=" + document.myForm.myText.value;
+	+ "&lang=" + langDirect
+	+ "&text=" + document.translateForm.translateText.value;
 	var xhr = new XMLHttpRequest();
 
 	xhr.open('GET', url, false);
-	console.log("Send request: " + document.myForm.myText.value);
 	xhr.send();
 	if (xhr.status != 200) 
 	{
-		console.log( xhr.status + ': ' + xhr.statusText );
+		document.getElementById('translateResult').innerHTML = "Some thing wrong...";
 	} 
 	else 
 	{
 		var xmlReq = xhr.responseXML;
 		var enWord = xmlReq.getElementsByTagName("text")[0].innerHTML;
 		var ruWord = xmlReq.getElementsByTagName("text")[1].innerHTML;
-		//document.form.sub.value = ruWord;
-		document.getElementById('res').innerHTML = ruWord;
-		console.log("Get translate: " + ruWord);
+		document.getElementById('translateResult').innerHTML = ruWord;
 	}
-	console.log("---------------------");
 }			
